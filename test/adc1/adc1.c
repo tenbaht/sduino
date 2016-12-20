674601ca7a03c1612edec668bb49961165a9a925
Michael Mayer <michael-mayer@gmx.de>
2016-12-20 14:31:50 +0100

    test/adc1 works, TX pin is skipped

--- test/adc1/adc1.c
+++ test/adc1/adc1.c
@@ -1,11 +1,13 @@
 /*
  * test adc1, read all five channels
+ *
+ * But skip channel 4, as it is used as TX. analogRead would mess up the
+ * send data while the ADC is active.
  */
 
 #include "Arduino.h"
 #include "Serial.h"
 
-#include "stm8s_flash.h"
 
 void setup(void)
 {
@@ -16,19 +18,22 @@ void setup(void)
 	pinMode(A0, INPUT);
 	pinMode(A1, INPUT);
 	pinMode(A2, INPUT);
-	pinMode(A3, INPUT);
-	pinMode(A4, INPUT);
+	pinMode(A3, INPUT);	// this pin is used as TX
+	pinMode(A4, INPUT);	// this pin is used as RX
 }
 
 
 void loop (void)
 {
 	uint8_t i;
+	uint16_t val;
 
-	for (i=0; i<5; ++i) {
+	for (i=0; i<NUM_ANALOG_INPUTS; ++i) {
 		// use channel numbers for reading the inputs
-		Serial_print_u(analogRead(i));
-		printStr("\t");
+		// skip channel 3 (used as TX), use dummy value 1234 instead
+		val = (i==3) ? 1234 : analogRead(i);
+		Serial_print_u(val);
+		printChr('\t');
 	};
 	println();
 
