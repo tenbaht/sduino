cdd4b0b9cf8c4495a9befa4e0b18380bea0d5c19
Michael Mayer <michael-mayer@gmx.de>
2016-12-22 12:42:06 +0100

    added pinout for STM8S103 breakout board

--- sduino/pins_arduino.h
+++ sduino/pins_arduino.h
@@ -167,45 +167,23 @@ extern const uint8_t digitalPinToAnalogChannelMap[];
 
 #ifdef ARDUINO_MAIN
 
-// On the Arduino board, digital pins are also used
-// for the analog output (software PWM).  Analog input
-// pins are a separate set.
-
-// ATMEL ATMEGA8 & 168 / ARDUINO
+// STM8S103F3 breakout board
 //
-//                  +-\/-+
-//            PC6  1|    |28  PC5 (AI 5)
-//      (D 0) PD0  2|    |27  PC4 (AI 4)
-//      (D 1) PD1  3|    |26  PC3 (AI 3)
-//      (D 2) PD2  4|    |25  PC2 (AI 2)
-// PWM+ (D 3) PD3  5|    |24  PC1 (AI 1)
-//      (D 4) PD4  6|    |23  PC0 (AI 0)
-//            VCC  7|    |22  GND
-//            GND  8|    |21  AREF
-//            PB6  9|    |20  AVCC
-//            PB7 10|    |19  PB5 (D 13)
-// PWM+ (D 5) PD5 11|    |18  PB4 (D 12)
-// PWM+ (D 6) PD6 12|    |17  PB3 (D 11) PWM
-//      (D 7) PD7 13|    |16  PB2 (D 10) PWM
-//      (D 8) PB0 14|    |15  PB1 (D 9) PWM
-//                  +----+
+//                       +-\/-+
+//          (D 13) PD4  1|    |20  PD3 (D 12, AI 2) PWM
+// TX (D 14, AI 3) PD5  2|    |19  PD2 (D 11, AI 1)
+// RX (D 15, AI 4) PD6  3|    |18  PD1 (D 10)
+//                NRST  4|    |17  PC7 (D 9)   MISO/PWM+
+//           (D 0) PA1  5|    |16  PC6 (D 8)   MOSI/PWM+
+//           (D 1) PA2  6|    |15  PC5 (D 7)    SCK/PWM+
+//                 GND  7|    |14  PC4 (D 6, AI 0)  PWM
+//                Vcap  8|    |13  PC3 (D 5)        PWM
+//                 Vdd  9|    |12  PB4 (D 4)        SCL
+// PWM       (D 2) PA3 10|    |11  PB5 (D 3)   LED, SDA
+//                       +----+
 //
-// (PWM+ indicates the additional PWM pins on the ATmega168.)
-
-// ATMEL ATMEGA1280 / ARDUINO
-//
-// 0-7 PE0-PE7   works
-// 8-13 PB0-PB5  works
-// 14-21 PA0-PA7 works 
-// 22-29 PH0-PH7 works
-// 30-35 PG5-PG0 works
-// 36-43 PC7-PC0 works
-// 44-51 PJ7-PJ0 works
-// 52-59 PL7-PL0 works
-// 60-67 PD7-PD0 works
-// A0-A7 PF0-PF7
-// A8-A15 PK0-PK7
-
+// (PWM+ indicates the additional PWM pins on alternate
+// function pins)
 
 // these arrays map port names (e.g. port B) to the
 // appropriate addresses for various functions (e.g. reading
