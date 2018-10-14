4388a64e8b42116ee74326bfe68fda227e73002e
Georg Icking-Konert <icking@onlinehome.de>
2018-10-14 19:22:56 +0200

    add LCD output
    add LCD output

--- sduino/hardware/sduino/stm8/libraries/muBoard/src/muBoard.h
+++ sduino/hardware/sduino/stm8/libraries/muBoard/src/muBoard.h
@@ -3,13 +3,20 @@
 
 #include <stdio.h>
 #include <string.h>
-//#include <inttypes.h>
 #include "Arduino.h"
-#include "beep.h"
+#include "muBoard_gpio.h"
+#include "muBoard_beep.h"
+#include <LCD_BTHQ21605V.h>
+
 
 // HW version v2.2 w/o CAN, or v2.3 with CAN
 //#define HW_VERSION 22
 #define HW_VERSION 23
+
+// LCD display configuration
+#define LCD_I2C_ADDR    0x3B    // I2C address  
+#define LCD_RST_PORT    GPIOE  	// reset port
+#define LCD_RST_PIN     3       // reset pin 	
 
 /// macro for HV-bridge control (BRIDGE_1-4 correspond to digital pins 26-29; True = ON = output high)
 #define set_bridge(num,state)		digitalWrite(25+num, state)
