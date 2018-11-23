aefb09e86f7a5fe5e0f066e155beb000ff428fd0
Michael Mayer <michael-mayer@gmx.de>
2018-11-23 11:05:46 +0100

    add docs for stm8sblack

---
+++ docs/hardware/stm8sblack.md
@@ -0,0 +1,154 @@
+# Generic STM8S105 breakout board
+
+These simple breakout boards are are available on aliexpress and the other
+usual china web sites as "stm8s105k4t6 development board" for $1.60
+including shipping.
+
+STM8S105K4T6 microcontroller, 16 KB Flash, 2 KB RAM, 1 KB EEPROM 
+
+  - LED on PE5, active low.
+  - external crystal 8MHz on PA1/PA2 or internal oszillator with 16 MHz
+  - build-in serial bootloader
+  - all I/O-pins accessible on header pins
+  - mounting position for 4 pin SWIM connector (for ST-Link flash tool)
+  - micro-USB connector for power supply only (data lines not connected)
+  - 3.3V I/O voltage, regulator on board
+
+
+![Picture of the STM8S105K board](stm8sblack.jpg)
+
+These boards are the bigger brother of the [STM8S103 breakout
+boards](stm8blue.md) with more I/O lines, more flash and more EEPROM. They
+are build around a CPU STM8S105K4T6 with 16MHz internal oscillator, 16kB
+flash, 2kB RAM, and 1kB EEPROM. The CPU includes a UART, SPI, I2C, PWM, 10
+bit ADC, 3 timer, and up to 25 I/O pins - almost like an Arduino Nano, but
+only half the flash space.
+
+One (red) LED is connected to GPIO PE5. The push button is for reset. The
+CPU runs on 3.3V, a linear regulator is integrated on the board. The micro
+USB connector is only for (5V) power supply, the data lines are not
+connected.
+
+All CPU pins are easily accessible on (optional) pin headers (pitch 2.54mm,
+perfect for breadboards).
+
+
+![Schematic of the STMS105 board](stm8sblack-schematic.jpg)
+
+
+## I2C not easy accessible
+
+Unfortunatly, I2C is only available as an alternate pin function on this
+CPU. So far, there is no support in Sduino for handling the alternate
+function remapping bits in the option byte.
+
+It is possible to modify these configuration registers via SWIM using a
+flash tool and stm8flash, but you are on your own there.
+
+
+
+## Connection to the flash tool
+
+I am using the ST-Link V2 compatible flash tool in the green plastic
+housing. The one in the metal housing uses a different pinout.
+
+Connection to the flashtool:
+
+Signal name	|P3 on CPU board	|Green flash tool|Metal flash tool
+------ 		|:-----:		|:-----: 	|:-----:
+3V3    		|1      		|2      	| 7
+SWIM   		|2      		|5      	| 5
+GND    		|3      		|7      	| 3
+NRST   		|4      		|9      	| 1
+
+
+
+## Pin number mappings
+
+The Arduino environment uses its own pin numbering scheme independent from
+the physical CPU pin numbers. Many Arduino sketches and libraries contain
+hard-coded assumptions about the number of pins with special functions.
+Ideally, all these numbers would be the same and all programs could be
+compiled without changes.
+
+Unfortunatly, it turns out that a perfect mapping is not possible.
+
+In the end I chose a simple geometric numbering for the module. The
+numbering is similar to a classic DIP package with the USB connector as the
+pin 1 marking. It starts with port pin PC3 and counting up from 0. This
+results in this mapping:
+
+![STM8S105 breakout board pin mapping](stm8s_black_pinout.png)
+
+
+
+sduino pin	| STM8S105 CPU port pin
+----------	| ---------------------
+ 0-4		| PC3-PC7
+ 5-12		| PD0-PD7
+13,14		| PA1-PA2, connected to the 8 MHz crystal, weak output drivers
+16-21		| PB5-PB0 (reverse order, weak output drivers)
+ 22		| PE5, weak output driver
+ 23,24		| PC1-PC2, weak output drivers
+
+- serial: 10,11
+- SPI: 2,3,4,22
+- I2C: 16,17 (has to be activated as an alternative function first)
+- Analog: 16-21
+- PWM: 0,1,5,7-9,23,24
+- LED: 22
+
+pros of this approach:
+
+- Easy and logical for use on a breadboard
+- Very clear and logical port pin ordering
+
+cons of this approach:
+
+- All functions use totally different pin numbers than Arduino
+
+I am still not really happy with this mapping. Instead of simplifing things
+it only adds another layer of abstraction and confusion. To avoid this I
+added definitions for the regular CPU pin names like `PA1` and `PD2`. In the
+end, this notation seems to be a lot easier to me. I am open for suggestions
+for a better pin number mapping.
+
+
+The chosen pin mapping for the STM8S105K (possible alternate function in
+paratheses):
+
+|Phys. STM8 pin|Name	|Functions	|Geometrical mapping|special funcion
+|---:	|---	|---			| ---	|---
+|2	|PA1	|(OscIn, no HS)		|13	|
+|3	|PA2	|(OscOut, no HS)	|14	|
+|8	|PF4	|Ain12 (not supported, no HS)|15	|
+|11	|PB5	|Ain5, [SDA], no HS	|16	|Analog A0
+|12	|PB4	|Ain4, [SCL], no HS	|17	|Analog A1
+|13	|PB3	|Ain3, no HS		|18	|Analog A2
+|14	|PB2	|Ain2, no HS		|19	|Analog A3
+|15	|PB1	|Ain1, no HS		|20	|Analog A4
+|16	|PB0	|Ain0, no HS		|21	|Analog A5
+|17	|PE5	|SPI_NSS, no HS		|22	|LED
+|18	|PC1	|T1-1			|23	|PWM
+|19	|PC2	|T1-2			|24	|PWM
+|20	|PC3	|T1-3			|0	|PWM
+|21	|PC4	|T1-4			|1	|PWM
+|22	|PC5	|SCK			|2	|
+|23	|PC6	|MOSI			|3	|
+|24	|PC7	|MISO			|4	|
+|25	|PD0	|T3-2			|5	|PWM
+|26	|PD1	|SWIM			|6	|
+|27	|PD2	|T3-1			|7	|PWM
+|28	|PD3	|T2-2			|8	|PWM
+|29	|PD4	|T2-1/Beep		|9	|PWM
+|30	|PD5	|TX			|10	|
+|31	|PD6	|RX			|11	|
+|32	|PD7	|TLI			|12	|
+
+
+
+## Special pins
+
+Most pins on the right side are weaker than the other pins. Try avoiding
+these pins for LEDs and other higher current applications: D13-D22 (PA1/PA2,
+PF4, PB5-PB0, PE5)
--- sduino/hardware/sduino/stm8/variants/stm8sblack/pins_arduino.h
+++ sduino/hardware/sduino/stm8/variants/stm8sblack/pins_arduino.h
@@ -29,8 +29,8 @@
 
 
 #define NUM_DIGITAL_PINS            25
-#define NUM_ANALOG_INPUTS           9
-#define analogInputToDigitalPin(p)  ((p < NUM_ANALOG_INPUTS) ? (p) + (NUM_DIGITAL_PINS-NUM_ANALOG_INPUTS) : -1)
+#define NUM_ANALOG_INPUTS           6
+#define analogInputToDigitalPin(p)  ((p < NUM_ANALOG_INPUTS) ? 16+(p) : -1)
 
 
 /* strict geometrical pin mapping for the STM8S105K4T6 breakout board.
@@ -39,49 +39,44 @@
  * all pin numbers are totally different than on any other board. So all
  * existing sketches will require adopted pin numbers.
  *
- * Potential pitfall: There is an overlap of the number ranges of analog
- * channel numbers and digital pin numbers for pins 3..5. Values of 3..5
- * will be treated as channel numbers, not as digital pin numbers.
  */
 enum portpin {
-	PA1, /* 0	OSC IN			*/
-	PA2, /* 1	OSC OUT			*/
-	PF4, /* 2	AIN12			*/
-	PB5, /* 3	AIN5 / [I2C_SDA]	*/
-	PB4, /* 4	AIN4 / [I2C_SCL]	*/
-	PB3, /* 5	AIN3 / [TIM1_ETR]	*/
-	PB2, /* 6	AIN2 / [TIM2_CH3N]	*/
-	PB1, /* 7	AIN1 / [TIM1_CH2N]	*/
-	PB0, /* 8	AIN0 / [TIM1_CH1N]	*/
-	PE5, /* 9	SPI_NSS			*/
-	PC1, /* 10~	TIM1_CH1 / UART2_CK	*/
-	PC2, /* 11~	TIM1_CH2		*/
+	PC3, /* 0~	TIM1_CH3		*/
+	PC4, /* 1~	TIM1_CH4		*/
+	PC5, /* 2	SPI_SCK			*/
+	PC6, /* 3	SPI_MOSI		*/
+	PC7, /* 4	SPI_MISO		*/
+	PD0, /* 5~	TIM3_CH2 / [TIM1_BKIN] / [CLK_CCO] */
+	PD1, /* 6	SWIM			*/
+	PD2, /* 7~	TIM3_CH1 / [TIM2_CH3]	*/
+	PD3, /* 8~	TIM2_CH2 / [ADC_ETR]	*/
+	PD4, /* 9~	TIM2_CH1 / [BEEP]	*/
+	PD5, /* 10	UART2_TX		*/
+	PD6, /* 11	UART2_RX		*/
 	PD7, /* 12	TLI / [TIM1_CH4]	*/
-	PD6, /* 13	UART2_RX		*/
-	PD5, /* 14	UART2_TX		*/
-	PD4, /* 15~	TIM2_CH1 / [BEEP]	*/
-	PD3, /* 16~	TIM2_CH2 / [ADC_ETR]	*/
-	PD2, /* 17~	TIM3_CH1 / [TIM2_CH3]	*/
-	PD1, /* 18	SWIM			*/
-	PD0, /* 19~	TIM3_CH2 / [TIM1_BKIN] / [CLK_CCO] */
-	PC7, /* 20	SPI_MISO		*/
-	PC6, /* 21	SPI_MOSI		*/
-	PC5, /* 22	SPI_SCK			*/
-	PC4, /* 23~	TIM1_CH4		*/
-	PC3, /* 24~	TIM1_CH3		*/
+	PA1, /* 13	OSC IN			*/
+	PA2, /* 14	OSC OUT			*/
+	PF4, /* 15	AIN12			*/
+	PB5, /* 16	AIN5 / [I2C_SDA]	*/
+	PB4, /* 17	AIN4 / [I2C_SCL]	*/
+	PB3, /* 18	AIN3 / [TIM1_ETR]	*/
+	PB2, /* 19	AIN2 / [TIM2_CH3N]	*/
+	PB1, /* 20	AIN1 / [TIM1_CH2N]	*/
+	PB0, /* 21	AIN0 / [TIM1_CH1N]	*/
+	PE5, /* 22	SPI_NSS			*/
+	PC1, /* 23~	TIM1_CH1 / UART2_CK	*/
+	PC2, /* 24~	TIM1_CH2		*/
 };
 
 
+// PWM on pins 0,1,5,7-9,23,24
+//#define digitalPinHasPWM(p)	( ((p)>=6&(p)<=9) | ((p)>=11&(p)<=13) | ((p)>=16&(p)<=18) | (p)==20 | (p)==24 | (p)==25 |)
+#define digitalPinHasPWM(p)	( ((p)<2) | ((p)==5) | ((p)>=7&(p)<=9) | ((p)==23) | ((p)==24) )
 
-// PWM on pins 12-13,13,25-28
- // using alternate functions adds 3 more PWM pins, total of 7 PWM pins
-#define digitalPinHasPWM(p)	( ((p)>=6&(p)<=9) | ((p)>=11&(p)<=13) | ((p)>=16&(p)<=18) | (p)==20 | (p)==24 | (p)==25 |)
-
-
-#define PIN_SPI_SS    (PE5)	// 11
-#define PIN_SPI_MOSI  (PC6)	// 15
-#define PIN_SPI_MISO  (PC7)	// 16
-#define PIN_SPI_SCK   (PC5)	// 14
+#define PIN_SPI_SS    (PE5)	// 22
+#define PIN_SPI_MOSI  (PC6)	//  3
+#define PIN_SPI_MISO  (PC7)	//  4
+#define PIN_SPI_SCK   (PC5)	//  2
 
 /* SDCC workaround: These const variables wouldn't be replaced by hard
  * constant loads. So use defines instead.
@@ -95,8 +90,8 @@ static const uint8_t SCK  = PIN_SPI_SCK;
 #define	MISO	PIN_SPI_MISO
 #define	SCK	PIN_SPI_SCK
 
-#define PIN_WIRE_SDA        (PB5)	// 3
-#define PIN_WIRE_SCL        (PB4)	// 4
+#define PIN_WIRE_SDA        (PB5)	// 17
+#define PIN_WIRE_SCL        (PB4)	// 16
 
 /* SDCC workaround
 static const uint8_t SDA = PIN_WIRE_SDA;
@@ -105,23 +100,19 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 #define	SDA	PIN_WIRE_SDA
 #define	SCL	PIN_WIRE_SCL
 
-#define PIN_LED_BUILTIN (PE5)	// sduino: pin for the buildin LED, pin 13
-#define PIN_TX	(PD5)		// sduino: pin for TX line, pin 8
-#define PIN_RX	(PD6)		// sduino: pin for RX line, pin 7
+#define PIN_LED_BUILTIN (PE5)	// sduino: pin for the buildin LED, pin 22
+#define PIN_TX	(PD5)		// sduino: pin for TX line, pin 13
+#define PIN_RX	(PD6)		// sduino: pin for RX line, pin 14
 
-#define LED_BUILTIN (PE5)	// pin for the buildin LED, pin 13
+#define LED_BUILTIN (PE5)	// pin for the buildin LED, pin 22
 
 // ADC (digital) pin numbers
-// note that for A0..A2 these numbers overlap with the channel numbers
-// and the values would be misstaken for an ADC channel number.
-// This is a serious problem with the strict geometrical mapping of the pin
-// numbers.
-#define PIN_A0   (PB5)		// 3, Ain5
-#define PIN_A1   (PB4)		// 4, Ain4
-#define PIN_A2   (PB3)		// 5, Ain3
-#define PIN_A3   (PB2)		// 6, Ain2
-#define PIN_A4   (PB1)		// 7, Ain1
-#define PIN_A5   (PB0)		// 8, Ain0
+#define PIN_A0   (PB5)		// 16, Ain5
+#define PIN_A1   (PB4)		// 17, Ain4
+#define PIN_A2   (PB3)		// 18, Ain3
+#define PIN_A3   (PB2)		// 19, Ain2
+#define PIN_A4   (PB1)		// 20, Ain1
+#define PIN_A5   (PB0)		// 21, Ain0
 
 /* SDCC workaround
 static const uint8_t A0 = PIN_A0;
@@ -141,11 +132,9 @@ static const uint8_t A4 = PIN_A4;
 #define NO_ANALOG	0xff
 
 // Distinguish between ADC channel number and digital pin number.
-// Note that for values 3..5 both ranges overlap and it is used a channel
-// number.
 //
 // values 0..5: ADC channel number, no conversion
-// values 6..8: digital pin numbers, convert to ADC channel number
+// values 16..21: digital pin numbers, convert to ADC channel number
 #define analogPinToChannel(P) (	(P)<=5 ? (P) : ( \
 					(P)<=PIN_A5 ? PIN_A5-(P) : NO_ANALOG \
 				))
@@ -163,29 +152,25 @@ extern const uint8_t digitalPinToAnalogChannelMap[];
 #ifdef ARDUINO_MAIN
 
 // STM8S105K5 breakout board
+//
+//                       +-/--\-+
+//                 GND  1|      |30  GND
+//                  5V  2|      |29  3V3
+//     PWM  (D0~)  PC3  3|      |28  PC2 (D 24~)     PWM
+//     PWM  (D1~)  PC4  4|      |27  PC1 (D 23~)     PWM
+//     SCK  (D 2)  PC5  5|      |26  PE5 (D 22)      LED NSS
+//     MOSI (D 3)  PC6  6|      |25  PB0 (D 21,AI 5)
+//     MISO (D 4)  PC7  7|      |24  PB1 (D 20,AI 4)
+//     PWM  (D 5~) PD0  8|      |23  PB2 (D 19,AI 3)
+//     SWIM (D 6)  PD1  9|      |22  PB3 (D 18,AI 2)
+//     PWM  (D 7~) PD2 10|      |21  PB4 (D 17,AI 1) SCL
+//     PWM  (D 8~) PD3 11|      |20  PB5 (D 16,AI 0) SDA
+//     PWM  (D 9~) PD4 12|      |19  PF4 (D 15)
+//     TX   (D 10) PD5 13|      |18  PA2 (D 14)
+//     RX   (D 11) PD6 14|      |17  PA1 (D 13)
+//          (D 12) PD7 15|      |16  NRST             
 
-// STM8S103F3 breakout board
-//
 //                       +-oooo-+
-//                NRST  1|      |30  PD7 (D 24)  PWM+
-//           (D 0) PA1  2|      |29  PD6 (D 23)  RX
-//           (D 1) PA2  3|      |27  PD5 (D 22)  TX
-//           (D 2) PF4  4|      |27  PD4 (D 21~) PWM
-// SDA  (D 3,AI 0) PB5  5|      |26  PD3 (D 20~) PWM
-// SCL  (D 4,AI 1) PB4  6|      |25  PD2 (D 19~) PWM
-//      (D 5,AI 2) PB3  7|      |24  PD1 (D 18)
-// PWM+ (D 6,AI 3) PB2  8|      |23  PD0 (D 17~) PWM
-// PWM+ (D 7,AI 4) PB1  9|      |22  PC7 (D 16)  MISO
-// PWM+ (D 8,AI 5) PB0 10|      |21  PC6 (D 15)  MOSI
-// LED NSS   (D 9) PE5 11|      |20  PC5 (D 14)  SCK
-// PWM     (D 10~) PC1 12|      |19  PC4 (D 13~) PWM
-// PWM     (D 11~) PC2 13|      |18  PC3 (D 12~) PWM
-//                 3V3 14|      |17  5V
-//                 GND 15|      |16  GND
-//                       +-\__/-+
-//
-// (PWM+ indicates the additional PWM pins on alternate
-// function pins)
 
 // these arrays map port names (e.g. port B) to the
 // appropriate addresses for various functions (e.g. reading
@@ -239,97 +224,87 @@ const uint16_t PROGMEM port_to_input_PGM[] = {
 };
 
 const uint8_t PROGMEM digital_pin_to_port_PGM[] = {
-	PA,   /* 0 */
-	PA,
-	PF,   /* 2 */
-	PB,   /* 3 */
-	PB, 
+	PC,   /* 0 */
+	PC,
+	PC,
+	PC,
+	PC,   /* 4 */
+	PD,   /* 5 */
+	PD,
+	PD,
+	PD,
+	PD,
+	PD,
+	PD,
+	PD,  /* 12 */
+	PA,  /* 13 */
+	PA,  /* 14 */
+	PF,  /* 15 */
+	PB,  /* 16 */
 	PB,
 	PB,
 	PB,
 	PB,
-	PE,  /* 9 */
-	PC,  /* 10 */
-	PC,
-	PC,
-	PC,
-	PC,
-	PC,
-	PC,
-	PD,  /* 15 */
-	PD,
-	PD,
-	PD,
-	PD,
-	PD,
-	PD,
-	PD,  /* 24 */
+	PB,  /* 21 */
+	PE,  /* 22 */
+	PC,  /* 23 */
+	PC,  /* 24 */
 };
 
 const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = {
-	_BV(1), /* 0, port A */
-	_BV(2),
-	_BV(4), /* 2, port F */
-	_BV(5), /* 3, port B */
-	_BV(4),
-	_BV(3),
-	_BV(2),
-	_BV(1),
-	_BV(0), 
-	_BV(5), /* 9, port E */ 
-	_BV(1), /* 10, port C */
-	_BV(2),
-	_BV(3),
-	_BV(4),
-	_BV(5), 
-	_BV(6),
-	_BV(7),
-	_BV(0), /* 17, port D */
-	_BV(1),
-	_BV(2),
-	_BV(3),
+	_BV(3), /* 0, port C */
 	_BV(4),
 	_BV(5),
 	_BV(6),
 	_BV(7),
+	_BV(0), /* 5, port D */
+	_BV(1),
+	_BV(2),
+	_BV(3), 
+	_BV(4),
+	_BV(5),
+	_BV(6),
+	_BV(7),
+	_BV(1), /* 13, port A */
+	_BV(2), 
+	_BV(4), /* 15, port F */
+	_BV(5), /* 16, port B */
+	_BV(4),
+	_BV(3),
+	_BV(2),
+	_BV(1),
+	_BV(0),
+	_BV(5), /* 22, port E */
+	_BV(1), /* 23, port C */
+	_BV(2),
 };
 
 const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
-	NOT_ON_TIMER,	// 0
-	NOT_ON_TIMER,	// 1
+	TIMER13,	// 0
+	TIMER14,	// 1
 	NOT_ON_TIMER,	// 2
 	NOT_ON_TIMER,	// 3
 	NOT_ON_TIMER,	// 4
-	NOT_ON_TIMER,	// 5
-#ifdef SUPPORT_ALTERNATE_MAPPINGS
-	TIMER13,	// 6
-	TIMER12,	// 7
-	TIMER11,	// 8
-#else
+	TIMER32,	// 5
 	NOT_ON_TIMER,	// 6
-	NOT_ON_TIMER,	// 7
-	NOT_ON_TIMER,	// 8
-#endif
-	NOT_ON_TIMER,	// 9
-	TIMER11,	// 10
-	TIMER12,	// 11
-	TIMER13,	// 12
-	TIMER14,	// 13
+	TIMER31,	// 7
+	TIMER22,	// 8
+	TIMER21,	// 9
+	NOT_ON_TIMER,	// 10
+	NOT_ON_TIMER,	// 11
+	NOT_ON_TIMER,	// 12
+	NOT_ON_TIMER,	// 13
 	NOT_ON_TIMER,	// 14
 	NOT_ON_TIMER,	// 15
 	NOT_ON_TIMER,	// 16
-	TIMER32,	// 17
+	NOT_ON_TIMER,	// 17
 	NOT_ON_TIMER,	// 18
-	TIMER31,	// 19
-	TIMER22,	// 20
-	TIMER21,	// 21
+	NOT_ON_TIMER,	// 19
+	NOT_ON_TIMER,	// 20
+	NOT_ON_TIMER,	// 21
 	NOT_ON_TIMER,	// 22
-	NOT_ON_TIMER,	// 23
-#ifdef SUPPORT_ALTERNATE_MAPPINGS
-	TIMER14,	// 24
-#else
-	NOT_ON_TIMER,	//24
-#endif
+	TIMER11,	// 23
+	TIMER12,	// 24
 };
 
 #endif
