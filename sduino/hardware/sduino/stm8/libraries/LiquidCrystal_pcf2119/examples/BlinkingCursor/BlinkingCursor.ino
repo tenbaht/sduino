a32e7d6a5ca927b8bbd6c4b8879b210d7e1ccbe3
gicking <icking@onlinehome.de>
2018-11-26 14:01:35 +0100

    added charset option
    - added option for non-ASCII charset
    - minor changes

--- sduino/hardware/sduino/stm8/libraries/LiquidCrystal_pcf2119/examples/BlinkingCursor/BlinkingCursor.ino
+++ sduino/hardware/sduino/stm8/libraries/LiquidCrystal_pcf2119/examples/BlinkingCursor/BlinkingCursor.ino
@@ -7,17 +7,20 @@
 #include <I2C.h> 
 #include <LiquidCrystal_pcf2119.h>
 
-#define LCD_ADDR  0x3B    // I2C address: 0x3A or 0x3B, dep. on pin SA0 state
-#define LCD_RST   PE3     // LCD reset pin (active high)
+// LCD controller settings
+#define LCD_ADDR    0x3B    // I2C address: 0x3A or 0x3B, dep. on pin SA0 state
+#define LCD_RST     PE3     // reset pin (active high)
+#define LCD_CHARSET 'R'     // character set (A, D, F, I, R or S)
 
 // Set LCD address and reset pin
-LiquidCrystal_pcf2119(lcd, LCD_ADDR, LCD_RST);
+LiquidCrystal_pcf2119(lcd, LCD_ADDR, LCD_RST, LCD_CHARSET);
 
 void setup()
 {
 	// initialize the LCD (16x2 display)
 	lcd_begin(16,2);
 }
+
 
 void loop()
 {
@@ -27,15 +30,15 @@ void loop()
 	while (1) {
 		if (blinking) {
 			lcd_clear();
-			lcd_print_s("No cursor blink");
+			lcd_print_s("Cursor static ");
 			lcd_noBlink();
 			blinking = false;
 		} else {
 			lcd_clear();
-			lcd_print_s("Cursor blink");
+			lcd_print_s("Cursor blink ");
 			lcd_blink();
 			blinking = true;
 		}
-		delay(4000);
+		delay(2500);
 	}
 }
