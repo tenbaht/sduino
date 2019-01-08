6d0fbd348d382696ab1774ee8a36ea9557022ffd
Michael Mayer <michael-mayer@gmx.de>
2019-01-08 12:20:08 +0100

    emit a warning if committed changes are present in the current source tree

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+### Added
+- a simple regression test script test/make-test.sh to compile all examples
+
 ### Changed
 - using busybox as command shell for windows to run the wrapper scripts
 
--- test/make-test.sh
+++ test/make-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CURDIR=$(pwd)
-SRCREV=$(git describe --tags --match="v*")
+SRCREV=$(git describe --tags --match="v*" --dirty)
 LOGFILE=${CURDIR}/logs/test-${SRCREV}.log
 BASEDIR=../sduino/hardware/sduino/stm8/libraries/
 
@@ -10,6 +10,7 @@ THREADS=$(grep processor  /proc/cpuinfo |wc -l)
 
 # make sure $COLUMNS is set
 shopt -s checkwinsize
+USE_COLOR=1
 
 print_status() {
 	local status
@@ -29,6 +30,35 @@ print_status() {
 	echo "compile log for all tests."
 	echo "source revision: $SRCREV"
 ) | tee "$LOGFILE"
+
+
+if [ $USE_COLOR -gt 0 ]; then
+# ANSI color codes to beautify the output:
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LGRAY='\033[0;37m'
+DGRAY='\033[1;30m'
+LRED='\033[1;31m'
+LGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LBLUE='\033[1;34m'
+LPURPLE='\033[1;35m'
+LCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+OFF='\033[0m'
+fi
+
+# check for a clean repository state
+if [[ "$SRCREV" = *-dirty ]]; then
+	echo -e "${ORANGE}Warning:${OFF}"\
+		"Uncommitted changes in the working tree. No clean repository state."
+fi
+
 
 # removing *all* old build directories for a clear start
 find "$BASEDIR" -name "build-*" -type d|xargs rm -r
