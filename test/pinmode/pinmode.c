85e9b6081dcc37c6ee7133660afaf837522afdc1
Michael Mayer <michael-mayer@gmx.de>
2019-01-09 13:42:22 +0100

    don't compile all the SDuino files

--- test/pinmode/pinmode.c
+++ test/pinmode/pinmode.c
@@ -2,10 +2,12 @@
  * test spi functions
  */
 
+#define ARDUINO_MAIN
 #include "Arduino.h"
 #include "wiring_private.h"
+#include "stm8s_it.h"
 
-void pinMode1(uint8_t pin, uint8_t mode)
+void pinMode_c(uint8_t pin, uint8_t mode)
 {
 	uint8_t bit = digitalPinToBitMask(pin);
 	uint8_t port = digitalPinToPort(pin);
@@ -266,7 +268,7 @@ uint8_t checkresult(uint8_t *adr, uint8_t *data)
 }
 
 
-void setup(void)
+void main(void)
 {
 	// expected result: xx xx 00 00 00
 	pinMode_asm(PA1,INPUT);
@@ -293,7 +295,30 @@ void setup(void)
 	checkresult(GPIOD_BaseAddress, "\x42\x02\x42");
 }
 
+#include "stm8s_it.h"
 
-void loop (void)
-{
-}
+/*
+ * empty default IRQ functions to make the linker happy if the
+ * respective module is not to linked.
+ */
+
+void UART1_TX_IRQHandler(void) __interrupt(ITC_IRQ_UART1_TX){}
+void UART1_RX_IRQHandler(void) __interrupt(ITC_IRQ_UART1_RX){}
+
+// define as __naked to avoid the "clr a/dix x,a" prolog
+#define IMPLEMENT_ISR(vect, interrupt) \
+ void vect(void) __interrupt((interrupt)>>8) __naked { \
+    __asm__("iret"); \
+  }
+
+IMPLEMENT_ISR(EXTI_PORTA_IRQHandler,		INT_PORTA) /* EXTI PORTA */
+IMPLEMENT_ISR(EXTI_PORTB_IRQHandler,		INT_PORTB) /* EXTI PORTB */
+IMPLEMENT_ISR(EXTI_PORTC_IRQHandler,		INT_PORTC) /* EXTI PORTC */
+IMPLEMENT_ISR(EXTI_PORTD_IRQHandler,		INT_PORTD) /* EXTI PORTD */
+IMPLEMENT_ISR(EXTI_PORTE_IRQHandler,		INT_PORTE) /* EXTI PORTE */
+IMPLEMENT_ISR(TIM1_CAP_COM_IRQHandler,		INT_TIM1_CAPCOM)
+//IMPLEMENT_ISR(TIM1_UPD_OVF_TRG_BRK_IRQHandler,	INT_TIM1_OVF)
+//IMPLEMENT_ISR(TIM2_CAP_COM_IRQHandler,		INT_TIM2_CAPCOM)
+//IMPLEMENT_ISR(TIM2_UPD_OVF_TRG_BRK_IRQHandler,	INT_TIM2_OVF)
+
+void TIM4_UPD_OVF_IRQHandler(void) __interrupt(ITC_IRQ_TIM4_OVF){}
